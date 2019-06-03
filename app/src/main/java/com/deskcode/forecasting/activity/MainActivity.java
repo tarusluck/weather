1d38c0577124960dcbce0ab7365edf886b0485e2
nikunj kakadiya <deskcodesolution3008@gmail.com>
2019-06-03 15:02:18 +0530

    2nd box UI Design

--- app/src/main/java/com/deskcode/forecasting/activity/MainActivity.java
+++ app/src/main/java/com/deskcode/forecasting/activity/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
